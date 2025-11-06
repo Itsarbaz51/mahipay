f0bb3368b79ad1c925a09e6939c459b58ccc6084
azzunique <azunisoftware18@gmail.com>
2025-11-06 18:59:09 +0530

    `fix: add null checks for optional properties in AddProfileKYC component and kycSlice.js`

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,17 +3,12 @@ dotenv.config({ path: "./.env" }); // Always load env first
 import Prisma from "./db/db.js";
 import app from "./app.js";
 import { envConfig } from "./config/env.config.js";
-import { redisConnection } from "./db/redis.js";
 
 (async function main() {
   try {
     console.log("Connecting to database...");
     await Prisma.$connect();
     console.log("✅ Database connected");
-
-    console.log("Connecting to Redis...");
-    await redisConnection();
-    console.log("✅ Redis is ready");
 
     const PORT = envConfig.PORT || 8000;
     app.listen(PORT, () => {
